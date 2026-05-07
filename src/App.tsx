8214499f4a2384f2b1af7486acb75477b2d50755
Ankit Jaiswal <codewithankitjaiswal@gmail.com>
2026-05-07 22:57:26 +0530

    Change root route to render Dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -29,10 +29,9 @@ const App = () => (
         <DataProvider>
           <BrowserRouter>
             <Routes>
-              <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
               <Route element={<AppLayout />}>
-                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/" element={<Dashboard />} />
                 <Route path="/notices" element={<Notices />} />
                 <Route path="/complaints" element={<ComplaintsPage scope="all" />} />
                 <Route path="/my-complaints" element={<ComplaintsPage scope="mine" />} />
